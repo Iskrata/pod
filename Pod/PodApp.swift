085797ee88a7a696168e37ca65a85481f2a209d4
Iskren Alexandrov <iskren.alexandrov@gmail.com>
2024-06-22 10:32:09 +0200

    Initial Commit

---
+++ Pod/PodApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodApp.swift
+//  Pod
+//
+//  Created by Iskren Alexandrov on 22.06.24.
+//
+
+import SwiftUI
+
+@main
+struct PodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
